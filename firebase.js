d47c060f35827a58c8043c8dec3a9f507969f269
Slaught11 <94186273+Slaught11@users.noreply.github.com>
2023-02-16 12:58:03 +0000

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,11 @@
+firebase.initializeApp({
+    apiKey: "AIzaSyAFu1l9TRUxT9PrhTEKk71POSvxt75Sksw",
+  authDomain: "fir-p5test.firebaseapp.com",
+  projectId: "fir-p5test",
+  storageBucket: "fir-p5test.appspot.com",
+  messagingSenderId: "792583088043",
+  appId: "1:792583088043:web:9e1d00a9153e8cc99214ee",
+  measurementId: "G-M38SD2GFLV"
+});
+
+var db = firebase.firestore()
